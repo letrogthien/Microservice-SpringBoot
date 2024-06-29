2fef42192c8cfd02b5c46c88bb50903695329ed0
le trong thien <letrogthien@gmail.com>
2024-06-29 22:37:43 +0700

    first

---
+++ authservice/src/main/java/com/example/demo/convert/ConvertUtils.java
@@ -0,0 +1,21 @@
+package com.example.demo.convert;
+
+import com.example.demo.dto.UserDto;
+import com.example.demo.models.User;
+import com.example.demo.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ConvertUtils {
+    private final ModelMapper modelMapper;
+
+    public User convertToUser(UserDto userDto){
+        return modelMapper.map(userDto,User.class);
+    }
+    public UserDto convertToUserDto(User user){
+        return modelMapper.map(user,UserDto.class);
+    }
+}
